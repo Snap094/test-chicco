1e3002de7ccc35392f2f03c5ea418694f8fb4453
nikonchuk.pp <nom100snap@gmail.com>
2023-03-20 15:38:06 +0200

    add baseUrl and demo link

--- utils/products.ts
+++ utils/products.ts
@@ -5,121 +5,121 @@ export const PRODUCTS = <Product[]>[
         title: 'Armchair1',
         subtitle: 'Light single chair',
         price: 145,
-        image: '/chair-1.png'
+        image: '/test-chicco/chair-1.png'
     },
     {
         title: 'Armchair2',
         subtitle: 'Light single chair',
         price: 145,
-        image: '/chair-2.png'
+        image: '/test-chicco/chair-2.png'
     },
     {
         title: 'Armchair3',
         subtitle: 'Light single chair',
         price: 145,
-        image: '/chair-3.png'
+        image: '/test-chicco/chair-3.png'
     },
     {
         title: 'Armchair4',
         subtitle: 'Light single chair',
         price: 145,
-        image: '/chair-4.png'
+        image: '/test-chicco/chair-4.png'
     },
     {
         title: 'Armchair5',
         subtitle: 'Light single chair',
         price: 145,
-        image: '/chair-1.png'
+        image: '/test-chicco/chair-1.png'
     },
     {
         title: 'Armchair6',
         subtitle: 'Light single chair',
         price: 145,
-        image: '/chair-2.png'
+        image: '/test-chicco/chair-2.png'
     },
     {
         title: 'Armchair7',
         subtitle: 'Light single chair',
         price: 145,
-        image: '/chair-3.png'
+        image: '/test-chicco/chair-3.png'
     },
     {
         title: 'Armchair8',
         subtitle: 'Light single chair',
         price: 145,
-        image: '/chair-4.png'
+        image: '/test-chicco/chair-4.png'
     },
     {
         title: 'Armchair9',
         subtitle: 'Light single chair',
         price: 145,
-        image: '/chair-1.png'
+        image: '/test-chicco/chair-1.png'
     },
     {
         title: 'Armchair10',
         subtitle: 'Light single chair',
         price: 145,
-        image: '/chair-2.png'
+        image: '/test-chicco/chair-2.png'
     },
     {
         title: 'Armchair11',
         subtitle: 'Light single chair',
         price: 145,
-        image: '/chair-3.png'
+        image: '/test-chicco/chair-3.png'
     },
     {
         title: 'Armchair12',
         subtitle: 'Light single chair',
         price: 145,
-        image: '/chair-4.png'
+        image: '/test-chicco/chair-4.png'
     },
     {
         title: 'Armchair13',
         subtitle: 'Light single chair',
         price: 145,
-        image: '/chair-1.png'
+        image: '/test-chicco/chair-1.png'
     },
     {
         title: 'Armchair14',
         subtitle: 'Light single chair',
         price: 145,
-        image: '/chair-2.png'
+        image: '/test-chicco/chair-2.png'
     },
     {
         title: 'Armchair15',
         subtitle: 'Light single chair',
         price: 145,
-        image: '/chair-3.png'
+        image: '/test-chicco/chair-3.png'
     },
     {
         title: 'Armchair16',
         subtitle: 'Light single chair',
         price: 145,
-        image: '/chair-4.png'
+        image: '/test-chicco/chair-4.png'
     },
     {
         title: 'Armchair17',
         subtitle: 'Light single chair',
         price: 145,
-        image: '/chair-1.png'
+        image: '/test-chicco/chair-1.png'
     },
     {
         title: 'Armchair18',
         subtitle: 'Light single chair',
         price: 145,
-        image: '/chair-2.png'
+        image: '/test-chicco/chair-2.png'
     },
     {
         title: 'Armchair19',
         subtitle: 'Light single chair',
         price: 145,
-        image: '/chair-3.png'
+        image: '/test-chicco/chair-3.png'
     },
     {
         title: 'Armchair20',
         subtitle: 'Light single chair',
         price: 145,
-        image: '/chair-4.png'
+        image: '/test-chicco/chair-4.png'
     },
 
 ]
