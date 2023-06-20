eb2b2719f8af916ad23384c8efc251ba7ba97df5
nikonchuk.pp <nom100snap@gmail.com>
2023-06-20 16:27:26 +0300

    Updates

--- _nuxt/default.ae6308fe.js
+++
@@ -1,1 +0,0 @@
-import{_ as t}from"./Header.vue.7a3a4302.js";import{j as s,b as a,f as c,e as n,z as r,o as _}from"./entry.33bafae8.js";import"./nuxt-link.d6f5e165.js";const d={},l={class:"global-content"},f={class:"content"};function i(e,m){const o=t;return _(),a("div",l,[c(o),n("div",f,[r(e.$slots,"default")])])}const B=s(d,[["render",i]]);export{B as default};
